d5f808d72f25d399acbe8f8c20135917a3fa1e3d
anderc1-debug <anderc1@farmingdale.edu>
2026-02-24 15:59:07 -0500

    Delete src/main directory

--- src/main/java/module-info.java
+++
@@ -1,8 +0,0 @@
-module org.example.csc311_module3assignment3 {
-    requires javafx.controls;
-    requires javafx.fxml;
-
-
-    opens org.example.csc311_module3assignment3 to javafx.fxml;
-    exports org.example.csc311_module3assignment3;
-}
